00a10dd074fb7e90733e2ea57849fe45e9f93274
Benjy <benjyyeoh.wx@ynytechnology.com.my>
2025-11-21 00:54:49 +0800

    feat: TMDB api setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,15 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
 import { useRouter } from "expo-router";
 import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
+  const movies = fetchMovies({query: ""});
+
+  console.log("MOVIES: ", movies)
 
   return (
     <View className="flex-1 bg-primary">
