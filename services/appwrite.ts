c045eadb73e1a1d743db04429ac993492936bdd7
Benjy <benjyyeoh.wx@ynytechnology.com.my>
2025-11-30 16:20:31 +0800

    feat: show trending movies

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -51,3 +51,21 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
     throw error;
   }
 }
+
+export const getTredingMovies = async (): Promise<TrendingMovie[] | undefined> => {
+  try {
+    const result = await database.listRows({
+      databaseId: DATABASE_ID,
+      tableId: TABLE_ID,
+      queries: [
+        Query.limit(5),
+        Query.orderDesc('count'),
+      ]
+    })
+
+    return result.rows as unknown as TrendingMovie[];
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+}
