bc538fa5574b1e00f2d2307a66db1c22da9ce856
Benjy <benjyyeoh.wx@ynytechnology.com.my>
2025-11-30 16:30:45 +0800

    refactor: hide status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,22 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import "./global.css";
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen
-      name="(tabs)"
-      options={{ headerShown: false }}
-    />
+  return (
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false }}
+        />
 
-    <Stack.Screen 
-      name="movies/[id]"
-      options={{ headerShown: false }}
-    />
-  </Stack>;
+        <Stack.Screen 
+          name="movies/[id]"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </>
+  )
 }
